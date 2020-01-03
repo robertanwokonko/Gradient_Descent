7ce7dd3ed2f842e7c3dbbc0fee7b3203c34916d4
Roberta Nwokonko <chidystyle@gmail.com>
2020-01-03 23:37:44 +0100

    Update code

---
+++ Code.py
@@ -0,0 +1,49 @@
+import numpy as np
+
+def gradient_descent(x,y):
+    """
+    Gradient descent is an optimization algorithim thats finds
+    the best fit line for a given training data set.
+
+    This function uses an efficient approach
+    when finding the best permutation and combination of 
+    m & b in your cost function (In this case cost function 
+    is Mean Squared Error(MSE))
+
+    When taking the slope of a non-linear graph, use derivatives
+    when taking the slope of a linear graph, use the equation of a straight line
+
+    Arguments:
+    x - a collection of n p-dimensional feature vectors
+    y - a collection of observed responses.
+    """
+
+    m_curr = b_curr = 0
+    iterations = 1000
+    n = len(x)
+    learningrate= 0.001
+
+    for i in range(iterations):
+        # (y = mx + b)
+        y_predicted = m_curr * x + b_curr 
+
+
+        # The derivative of MSE wrt to m & b algorithim was taken because it tells you the
+        # slope everywhere in a non-linear set of features, which we need to 
+        # know the direction to go, to find the best gradient(change in y wrt to x). 
+        # Please remember that if m is the same everywhere, the problem of prediction 
+        # is solved
+       
+        dm = -(2/n)*sum(x*(y-y_predicted))
+        db = -(2/n)*sum(y-y_predicted)
+
+        # this step is to move towards your global minmum and update movements
+        m_curr = m_curr - learningrate* dm
+        b_curr = b_curr - learningrate* db
+        print('m {}, b {}, iteration{}'.format(m_curr,b_curr,i))
+
+# test
+x = np.array([1,2,3,4,5])
+y = np.array([5,7,9,11,13])
+
+gradient_descent(x,y)
